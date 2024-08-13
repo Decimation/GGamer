8cc87932bab282040ea87a7a182c2d06fd8d5893
Read Stanton <decimation001@gmail.com>
2024-08-13 10:22:33 -0500

    ?

--- GGamer/Program.cs
+++ GGamer/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System.Diagnostics;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using NSelenium;
+using NSelenium.Utilities;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using OpenQA.Selenium.DevTools.V127.Network;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using SeleniumUndetectedChromeDriver;
+using Cookie = OpenQA.Selenium.Cookie;
 using SCookie = OpenQA.Selenium.DevTools.V127.Network.Cookie;
 
 namespace GGamer;
@@ -11,7 +18,79 @@ namespace GGamer;
 public static class Program
 {
 
+	public class CollectionMonitor<T>
+	{
+
+		private readonly List<T>          _collection        = new List<T>();
+		private readonly Subject<List<T>> _collectionSubject = new Subject<List<T>>();
+
+		public CollectionMonitor()
+		{
+			_collectionSubject
+				.Select(collection => collection.Where(item => Predicate(item)).ToList())
+				.Where(subset => subset.Count > 0)
+				.Subscribe(subset => OnSubsetMatched(subset));
+		}
+
+		public Func<T, bool> Predicate { get; set; }
+
+		public event Action<List<T>> SubsetMatched;
+
+		protected virtual void OnSubsetMatched(List<T> subset)
+		{
+			SubsetMatched?.Invoke(subset);
+		}
+
+		public void AddItem(T item)
+		{
+			_collection.Add(item);
+			_collectionSubject.OnNext(_collection);
+		}
+
+		public void RemoveItem(T item)
+		{
+			_collection.Remove(item);
+			_collectionSubject.OnNext(_collection);
+		}
+
+		public void Clear()
+		{
+			_collection.Clear();
+			_collectionSubject.OnNext(_collection);
+		}
+
+		public async Task<bool> MonitorCollectionAsync(Func<T, bool> predicate, TimeSpan timeout)
+		{
+			Predicate = predicate;
+
+			var tcs = new TaskCompletionSource<List<T>>();
+
+			void Handler(List<T> subset)
+			{
+				tcs.TrySetResult(subset);
+			}
+
+			SubsetMatched += Handler;
+
+			var completedTask = await Task.WhenAny(tcs.Task, Task.Delay(timeout));
+
+			SubsetMatched -= Handler;
+
+			if (completedTask == tcs.Task) {
+				// Console.WriteLine("Subset matched: " + string.Join(", ", tcs.Task.Result));
+				return true;
+			}
+			else {
+				// Console.WriteLine("Timeout waiting for subset to match.");
+				return false;
+			}
+		}
+
+	}
+
 	private static UndetectedChromeDriver _driver;
+	private static DevToolsSession?       _devToolsSession;
+	private static NetworkAdapter         _network;
 
 	private const string URL1 = "https://www.chatgpt.com";
 
@@ -26,6 +105,9 @@ public static class Program
 
 		_driver = UndetectedChromeDriver.Create(co, driverExecutablePath:
 		                                        await new ChromeDriverInstaller().Auto());
+		_devToolsSession = _driver.GetDevToolsSession();
+		_network         = new NetworkAdapter(_devToolsSession);
+		await _network.Enable(new EnableCommandSettings());
 
 		await _driver.Navigate().GoToUrlAsync(URL1);
 
@@ -56,7 +138,28 @@ public static class Program
 		};
 		wdw2.IgnoreExceptionTypes([typeof(ElementNotInteractableException)]);
 
-		var hs = new HashSet<IWebElement>();
+		var hs   = new HashSet<IWebElement>();
+		var are  = new AutoResetEvent(false);
+		var reqs = new HashSet<Request>();
+
+		var cm = new CollectionMonitor<Request>()
+		{
+			Predicate = r => r.Url.Contains("https://chatgpt.com/backend-api/lat/r")
+		};
+
+		_network.RequestWillBeSent += (sender, e) =>
+		{
+			Debug.WriteLine($"{e.Request} {e.Request.Url}");
+			reqs.Add(e.Request);
+
+			/*
+			if (e.Request.Url.Contains("https://chatgpt.com/backend-api/lat/r")) {
+				// requestCompleted.SetResult(true);
+				are.Set();
+			}
+			*/
+			cm.AddItem(e.Request);
+		};
 
 		while (!cts.IsCancellationRequested) {
 			/*var comp = await _driver.WaitForRequest("r", TimeSpan.FromSeconds(1));
@@ -79,34 +182,54 @@ public static class Program
 
 			}*/
 
-			var msgs = wdw2.Until(x =>
-			{
-				return x.FindElements(By.XPath("//*[contains(@data-testid,'conversation-turn')]"));
-			}, cts.Token);
+			// var requestCompleted = new TaskCompletionSource<bool>();
+			/*
+			if (are.WaitOne(TimeSpan.FromSeconds(1))) {
+				var msgs = wdw2.Until(x =>
+				{
+					return x.FindElements(By.XPath("//*[contains(@data-testid,'conversation-turn')]"));
+				}, cts.Token);
 
-			foreach (IWebElement msg in msgs) {
-				/*var r=wdw2.WaitForElementToStopChanging(msg, TimeSpan.FromSeconds(3));
+				foreach (IWebElement msg in msgs) {
+					/*var r=wdw2.WaitForElementToStopChanging(msg, TimeSpan.FromSeconds(3));
 
 				if (r) {
 					Console.WriteLine($"{msg.Text}");
-				}*/
+				}#1#
 
-				/*if (msg.TryGetText(out var s)) {
+					/*if (msg.TryGetText(out var s)) {
 					Console.WriteLine(s);
+				}#1#
+					// _driver.WaitForLoad(TimeSpan.FromSeconds(1));
+
+
+					Console.WriteLine(msg.Text);
+				}
+
+			}
+			*/
+			if (await cm.MonitorCollectionAsync(cm.Predicate, TimeSpan.FromSeconds(5))) {
+				Console.WriteLine("subset match");
+
+				/*var msgs = wdw2.Until(x =>
+				{
+					return x.FindElements(By.XPath("//*[contains(@data-testid,'conversation-turn')]"));
+				}, cts.Token);
+
+				foreach (IWebElement msg in msgs) {
+					Console.WriteLine(msg.Text);
 				}*/
 
-				try {
-					if (hs.Add(msg)) {
-						Console.WriteLine(msg.Text);
-					}
-				}
-				catch (Exception e) {
-					// Console.WriteLine(e);
-					// throw;
-				}
+				var msg = wdw2.Until(x =>
+				{
+					return x.FindElement(By.XPath("//*[contains(@data-testid,'conversation-turn')]"));
+				}, cts.Token);
+
+				Console.WriteLine(msg.Text);
+				// cm.Clear();
 			}
 
-			await Task.Delay(TimeSpan.FromSeconds(1));
+			// await Task.Delay(TimeSpan.FromSeconds(1));
 			/*
 			foreach (var msg in msgs) {
 				if (!msg.IsStale() && !hs.Contains(msg)) {
@@ -117,10 +240,7 @@ public static class Program
 			}
 			*/
 
-
 			// wdw2.WaitForElementToStopChanging(By.XPath("//*[contains(@data-testid,'conversation-turn')]"), TimeSpan.FromSeconds(2));
-
-
 			// await Task.Delay(TimeSpan.FromSeconds(1));
 		}
 
@@ -229,17 +349,19 @@ public static class Program
 	public static async Task<bool> WaitForRequest(this ChromeDriver _chromeDriver, string urlPattern, TimeSpan timeout)
 	{
 		var devToolsSession  = _chromeDriver.GetDevToolsSession();
-		var network          = new OpenQA.Selenium.DevTools.V85.Network.NetworkAdapter(devToolsSession);
+		var network          = new NetworkAdapter(devToolsSession);
 		var requestCompleted = new TaskCompletionSource<bool>();
 
 		network.RequestWillBeSent += (sender, e) =>
 		{
+			Debug.WriteLine($"{e.Request} {e.Request.Url}");
+
 			if (e.Request.Url.Contains(urlPattern)) {
 				requestCompleted.SetResult(true);
 			}
 		};
 
-		await network.Enable(new OpenQA.Selenium.DevTools.V85.Network.EnableCommandSettings());
+		await network.Enable(new EnableCommandSettings());
 
 		var task = await Task.WhenAny(requestCompleted.Task, Task.Delay(timeout));
 
